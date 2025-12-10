8ce56d0a7554fb7b5b4744077b76a67e5ce50992
praneeth724 <tmkpraneeth@std.appsc.sab.ac.lk>
2025-12-10 12:13:32 +0530

    add google analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import NextAuthProvider from "@/components/NextAuthProvider";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -12,6 +13,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
+            <head>
+                <Script
+                    src="https://www.googletagmanager.com/gtag/js?id=G-K0CDNVXWHZ"
+                    strategy="afterInteractive"
+                />
+                <Script id="gtag-init" strategy="afterInteractive">
+                    {`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-K0CDNVXWHZ');
+                    `}
+                </Script>
+            </head>
             <body className={inter.className}>
                 <NextAuthProvider>
                     {children}
